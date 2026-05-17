0570af5cca4fd42b87b786afa490e7ed0d134fdf
Yanis <Yanis.r5ee@gmail.com>
2026-05-17 23:26:53 +0200

    Ajout du logo final

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     </div>
     
     <div class="logo-central">
-        <img src="../ressources/images/logo_S2M.png" alt="Logo Shop2Maillots">
+        <img src="../ressources/images/logo.png" alt="Logo Shop2Maillots">
     </div>
 
     <nav class="navigation-principale">
