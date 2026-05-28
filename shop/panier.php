78509206775adb89c16f55017964140ac44a8a52
Resfa781 <fares.t781@gmail.com>
2026-05-28 11:31:49 +0200

    SYSTEME DE PANNIER FOCNTIONELLE (AFFICHAGE SEULEMENT) ==== NEXT STEP : TENTER DE FAIRE FOCNTIONNER LA VALIDATION DU PANIER POUR AVOIR UNE NETE DE COMMANDE

--- shop/panier.php
+++ shop/panier.php
@@ -21,6 +21,45 @@
     
     <?php require_once("../includes/header.php"); ?>
 
+    <h2 class="titre-page">VOTRE PANIER</h2>
+
+    <div>
+        <?php 
+
+            $id_user = $_SESSION['id_user'];
+            $result = mysqli_query($con, "SELECT P.id_maillot, M.nom_maillot, M.fichier_image, P.quantite, P.numero, P.flocage, P.taille FROM panier P, user U, Maillot M WHERE P.id_maillot = M.id_maillot AND P.id_user = U.id_user AND U.id_user = '$id_user'");
+
+            while ($pannier = mysqli_fetch_array($result)) {
+                echo '<p class="nom-produit">'.$pannier['nom_maillot'].'</p>';
+                echo '<img src="../ressources/images/'.$pannier['fichier_image'].'" alt="'.$pannier['nom_maillot'].'" width="400" height="400" class="image-produit">';
+                echo '<p class="prix-produit"> Quantité : '.$pannier['quantite'].'</p><br>';
+
+                if (!empty($pannier['flocage'])) {
+                    echo '<p> Flocage : '.$pannier['flocage'].'</p><br>';
+                }
+
+                if (!empty($pannier['nuemro'])) {
+                    echo '<p> Flocage : '.$pannier['numero'].'</p><br>';
+                }
+
+                echo '<p class="prix-produit">29,99 &euro;</p>';
+                echo "<br>";
+            }
+        ?>
+    </div>
+
+
+
+    <form method="post" action="Valid_commande.php">
+        <input type="text" name="nom_commande" placeholder="Nom de la commande *" required>
+
+        <button type="submit">Valider le panier</button>
+    </form>
+
+    <hr>
+
+    <h2 class="titre-page">VOS COMMANDE</h2>
+
     <?php require_once("../includes/footer.php"); ?>
 
 </body>
