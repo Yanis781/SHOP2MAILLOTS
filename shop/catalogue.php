a7e198a55841f8091aa41ebe057cfb04d3718fdc
Yanis <Yanis.r5ee@gmail.com>
2026-06-03 12:47:12 +0200

    shop aéré commenté

--- shop/catalogue.php
+++ shop/catalogue.php
@@ -2,19 +2,25 @@
     session_start();
 
      /* -----  VERIF ET REDIRECTION DU USER POUR VOIR SI C L'ADMIN OU PAS ----- */
+     
     if (!isset($_SESSION['user'])) {
         header('location: ../index.php');
         exit();
     }
+
     /* -----  SI USER = ADMIN, ON LE REDIRIGE VERS LE DASHBORAD (LA PAGE D'ADMINISTRATION DU SITE) ----- */
+
     if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
         header('location: ../admin/dashboard.php');
         exit();
     }
+
     /* ------------------------------------------------------------------------ */
+
     require_once("../bdd/connect_db.php");
 
     /* ----- MISE EN PLACE DU SYSTEME DE RECHERCHE DANS LE CATALOGUE ----- */
+
     $titre_page = "CATALOGUE";
     $valeur_recherche = ""; 
 
@@ -44,13 +50,18 @@
     <link rel="icon" type="image/x-icon" href="../ressources/images/favicon.ico">
 </head>
 <body>
+
     <!---- AFFICHAGE DE L'ENTETE DE PAGE  ---->
+    
     <?php require_once("../includes/header.php"); ?>
 
     <!------ AFFICHAGE DU TITRE VARIABLE EN FONCTION DES RECHERCHES ----->
+
     <div class="conteneur-catalogue">
             <h1 class="titre-page"><?php echo $titre_page; ?></h1>
+
             <!---- BARRE DE RECHERCHE DE MAILLOT---->
+
            <div class="zone-recherche-catalogue">
                 <form method="GET" action="catalogue.php" class="barre-recherche">
                     <input type="text" name="recherche" class="input-recherche" placeholder="Rechercher un autre maillot..." value="<?php echo $valeur_recherche; ?>">
@@ -63,7 +74,9 @@
             <div class="grille-produits">
                 <?php 
                     if (mysqli_num_rows($result) > 0) {
+
                         /* ----- AFFICHAGE DES MAILLOTS EN FONCTION DE LA RECHERCHE ----- */
+
                         while ($maillot = mysqli_fetch_array($result)) {
                             echo '<div class="carte-produit">';
                             echo '  <img src="../ressources/images/'.$maillot['fichier_image'].'" class="image-produit" alt="'.$maillot['nom_maillot'].'">';
@@ -73,7 +86,9 @@
                             echo '  <a href="product.php?id-maillot='.$maillot['id_maillot'].'" class="bouton-voir">Voir le maillot</a>';
                             echo '</div>';
                         }
+
                     /* ------- SI LE MAILLOIT RECHERCHER N'EXISTE PAS, ON AFFICHE UN MESSAGE D'ERREUR AVEC UN BOUTON POUR RETOURNER AU CATALOGUE ------- */
+
                     } else {
                         echo '<div class="carte-produit">';
                         echo '<p style="text-align:center; width:100%;">Désolé, nous n\'avons pas trouvé de maillot correspondant.</p>';
